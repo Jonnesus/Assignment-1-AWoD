509f2c9367491105dc5bbfa615bf52c5024d5c0e
Jonnesus <bi24fz@student.sunderland.ac.uk>
2023-09-28 11:52:57 +0100

    V0.1.1

---
+++ Assets/Scripts/STUFF TO DO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class STUFFTODO : MonoBehaviour
+{
+    /* Fix DeathWall, Spikes + Portal collision
+     * Set up UI
+     * Main menu
+     * Background?
+     * Procedural generation (ONLY when all else is fixed)
+     */
+}
